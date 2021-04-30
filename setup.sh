79e9e1b27e6c69c552d465667e8ad978c89a352e
GingerCam <cameroncoull@gmail.com>
2021-04-30 19:19:11 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -10,10 +10,12 @@ echo "Do you want to continue?"
 echo "Press CTRL + C in the next 10 seconds to cancel"
 sleep 10
 
-echo "Radio-VNC will install hostapd, dnsmasq, GQRX, pixel desktop and all of their dependencies."
+echo "Radio-VNC will install hostapd, dnsmasq, GQRX, pixel desktop, vnc-server and all of their dependencies."
 
 apt update && apt upgrade -y 
-apt install -y hostapd dnsmasq gqrx-sdr raspberrypi-ui-mods curl wget
+apt install -y hostapd dnsmasq gqrx-sdr raspberrypi-ui-mods curl wget realvnc-vnc-server realvnc-vnc-viewer
+
+
 
 echo "Config files will be downloaded"
 
@@ -23,5 +25,18 @@ curl  https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/config/hostapd.
 
 echo ""DAEMON_CONF="/etc/hostapd/hostapd.conf" >> "/etc/default/hostapd"
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "sudo -u pi vncserver -randr=1400x900" >> /etc/rc.local
+systemctl unmask hostapd
+update-rc.d hostapd enable
+hostnamectl set-hostname 'Radio-VNC'
 
+echo"
+Network==Radio-VNC
+ip address==192.168.4.1
+hostname==Radio-VNC
+" >> /home/pi/info.txt
 
+echo "Radio-VNC is installed"
+echo "System will reboot in 5 seconds"
+sleep 5
+reboot
