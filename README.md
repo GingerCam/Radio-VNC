f2f09d2baa6e27040d4ceb6af25dbe6fedc9ebbe
GingerCam <cameroncoull@gmail.com>
2021-05-01 18:31:27 +0100

    lastest update

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Radio-VNC
 Radio-VNC is a project that allows you to control an SDR with your phone, you can control your pi by connecting to phone or laptop to the WI-FI network produced by the pi. Secondly you use a VNC client to connect to the Pi's IP address (192.168.4.1)
 It is recommended to change the default password for the Pi user.
-
 It uses GQRX to control the SDR and hostapd and dnsmasq for the access point
+
+# Requirements
+Any Raspberry Pi (The later ones are preferable, as SDR's use a bit of CPU)
+A USB SDR
+An aerial (For better reception)
+An active internet connection (For the setup process)
+A wireless interface (For the WI-FI network)
+Raspberry Pi OS lite installed on the pi
 
 # Easy install on Raspberry Pi
 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
@@ -14,7 +14,7 @@ sleep 10
 
 echo "Radio-VNC will install hostapd, dnsmasq, GQRX, pixel desktop, vnc-server and all of their dependencies."
 
-apt update && apt upgrade -y 
+apt update && apt upgrade -y
 apt install -y hostapd dnsmasq gqrx-sdr raspberrypi-ui-mods curl wget realvnc-vnc-server realvnc-vnc-viewer figlet
 
 
@@ -31,8 +31,30 @@ sleep 1
 echo "Setting up applications"
 echo ""DAEMON_CONF="/etc/hostapd/hostapd.conf" >> "/etc/default/hostapd"
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-echo "sudo -u pi vncserver -randr=1400x900" >> /etc/rc.local
 echo ""
+
+echo "Setting up wireless location to GB"
+wpa_cli -i wlan0 set country GB
+wpa_cli -i wlan0 save_config > /dev/null 2>&1
+rfkill unblock wifi
+for filename in /var/lib/systemd/rfkill*:wlan ; do
+  echo 0 > $filename
+done
+sleep 1
+
+echo "Setting up VNC server"
+systemctl enable vncserver-x11-serviced.service
+systemctl start vncserver-x11-serviced.service
+echo ""
+sleep 1
+echo "VNC server is configured"
+
+echo "Setting up ssh server"
+ssh-keygen -A
+update-rc.d ssh enable
+invoke-rc.d ssh start
+echo ""
+echo "ssh server is configured"
 
 echo "Setting up hostapd"
 sleep 1
@@ -45,7 +67,7 @@ hostnamectl set-hostname 'Radio-VNC'
 
 echo "Setting up GQRX to start on boot"
 sleep 1
-mkdir /home/pi/.config/autostart
+mkdir -p /home/pi/.config/autostart
 curl https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/other-files/gqrx.desktop -o /home/pi/.config/autostart/gqrx.desktop
 
 echo "Network==Radio-VNC | Network-Password==RaspberryRadio | ip address==192.168.4.1 | hostname==Radio-VNC" >> /home/pi/info.txt
