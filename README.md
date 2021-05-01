5d5b4ec5815289ec3a5a132a21f62d98fb65b56b
GingerCam <cameroncoull@gmail.com>
2021-05-01 14:40:46 +0100

    Added auto start function for GQRX

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 This project is for VNC without internet to control an SDR
 
 It uses GQRX to control the SDR and hostapd and dnsmasq for the access point
+
+Easy install on Raspberry Pi
+
+"curl https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/setup.sh | sudo bash"
+
+This project is being actively worked on by myself (GingerCam) and any feekback is greatly apreciated.
+
+It is recomended to install to a new SD card or USB drive so you don't loose any data. 
+
+To do list
+
+Add an uninstall script 
+Maybe make it an application rather than comsuming a whole SD card for this project
--- setup.sh
+++ setup.sh
@@ -22,17 +22,32 @@ echo "Config files will be downloaded"
 curl  https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/config/dhcpcd.conf -o /etc/dhcpcd.conf
 curl  https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/config/dnsmasq.conf -o /etc/dnsmasq.conf
 curl  https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/config/hostapd.conf -o /etc/hostapd/hostapd.conf
+echo "Config files have been downloaded"
+sleep 1
 
+echo "Setting up applications"
 echo ""DAEMON_CONF="/etc/hostapd/hostapd.conf" >> "/etc/default/hostapd"
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 echo "sudo -u pi vncserver -randr=1400x900" >> /etc/rc.local
+echo ""
+echo "Setting up hostapd"
+sleep 1
 systemctl unmask hostapd
 systemctl enable hostapd
+echo "Changing hostname to Radio-VNC"
+sleep 1
 hostnamectl set-hostname 'Radio-VNC'
+echo "Setting up GQRX to start on boot"
+sleep 1
+mkdir /home/pi/.config/autostart
+curl https://raw.githubusercontent.com/GingerCam/Radio-VNC/main/other-files/gqrx.desktop -o /home/pi/.config/autostart/gqrx.desktop
 
-echo"Network==Radio-VNC | Network-Password==RaspberryRadio | ip address==192.168.4.1 | hostname==Radio-VNC" >> /home/pi/info.txt
-
-figlet Radio-VNC is   installed
+echo "Network==Radio-VNC | Network-Password==RaspberryRadio | ip address==192.168.4.1 | hostname==Radio-VNC" >> /home/pi/info.txt
+echo "Check /home/pi/info.txt for more infomation"
+sleep 2
+echo ""
+echo ""
+figlet Radio-VNC is installed
 echo "System will reboot in 5 seconds"
 sleep 5
 reboot
