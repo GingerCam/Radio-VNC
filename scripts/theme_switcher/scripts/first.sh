41164339396d54aa45a7880aefe663978de3ef8f
GingerCam <cameroncoull@gmail.com>
2021-07-18 00:00:34 +0100

    Update first.sh

--- scripts/theme_switcher/scripts/first.sh
+++ scripts/theme_switcher/scripts/first.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 source /opt/Radio-VNC/functions.sh
+pwd=`pwd`
+errors=0
 
-if [(pwd) -ne /opt/Radio-VNC/]; then
+if [ $pwd -ne /opt/Radio-VNC/ ]; then
   echo "This script must be located in /opt/Radio-VNC."
+  errors=$((errors + 1))
+  
 fi
 
-if [ -f /usr/share/ThemeSwitcher/ ]; then
-  echo 1 
+if [ ! -f /usr/share/ThemeSwitcher/ ]; then
+  echo "/usr/share/ThemeSwitcher does not exist."
+  echo "Please reinstall ThemeSwitcher"
+  errors=$((errors + 1))
 fi 
+
+if [ $errors == 0 ]; then
+  /usr/share/ThemeSwitcher/ThemeSwitcher
+fi
